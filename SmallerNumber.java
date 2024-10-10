6ce15c6337c6949d5782d7395545722079527b93
Aditi-Masand <2022.aditi.masand@ves.ac.in>
2024-10-10 12:38:37 +0530

    Create SmallerNumber.java

---
+++ SmallerNumber.java
@@ -0,0 +1,14 @@
+public class SmallerNumber {
+    public static void main(String[] args) {
+        // Define two numbers
+        int num1 = 25;
+        int num2 = 15;
+        
+        // Compare and find the smaller number
+        if (num1 < num2) {
+            System.out.println("The smaller number is: " + num1);
+        } else {
+            System.out.println("The smaller number is: " + num2);
+        }
+    }
+}
